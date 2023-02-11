b14090682abf63be8d2eb27c131fb1eede9c9c77
Naffah Abdulla Rasheed <mail@naffah.me>
2023-02-11 13:50:18 +0500

    feat: Add a floating action button to show a dialog with a text field to add an Item into userList collection in Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shoplist/controllers/auth_controller.dart';
+import 'package:shoplist/controllers/item_list_controller.dart';
+import 'package:shoplist/models/item_model.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -45,6 +48,75 @@ class HomeScreen extends HookConsumerWidget {
                 icon: const Icon(Icons.logout),
               )
             : null,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => AddItemDialog.show(context, Item.empty()),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddItemDialog extends HookConsumerWidget {
+  final Item item;
+
+  const AddItemDialog({super.key, required this.item});
+
+  bool get isUpdating => item.id != null;
+
+  static void show(BuildContext context, Item item) {
+    showDialog(
+      context: context,
+      builder: (context) => AddItemDialog(item: item),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textController = useTextEditingController(text: item.name);
+
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: const InputDecoration(hintText: 'Item name'),
+            ),
+            const SizedBox(height: 12.0),
+            SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isUpdating
+                      ? Colors.orange
+                      : Theme.of(context).primaryColor,
+                ),
+                onPressed: () {
+                  isUpdating
+                      ? ref
+                          .read(itemListControllerProvider.notifier)
+                          .updateItem(
+                            updatedItem: item.copyWith(
+                              name: textController.text.trim(),
+                              obtained: item.obtained,
+                            ),
+                          )
+                      : ref
+                          .read(itemListControllerProvider.notifier)
+                          .addItem(name: textController.text.trim());
+
+                  Navigator.of(context).pop();
+                },
+                child: Text(isUpdating ? 'Update' : 'Add'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
